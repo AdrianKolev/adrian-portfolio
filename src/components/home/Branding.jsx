d6a989f020d680aa25155bf8a289bd5db487084d
Adrian Kolev <adrian.kolev@netea.bg>
2023-02-18 11:06:57 +0200

    small refinments on the UI

--- src/components/home/Branding.jsx
+++ src/components/home/Branding.jsx
@@ -4,17 +4,17 @@ const Branding = () => {
   const data = [
     {
       id: "01",
-      heading: "Digital Branding",
+      heading: "Define",
       desc: "Lorem ipsum dolor sit amet, provid adipisicing elit. Aliquam dolorum nu illo similique! Impedit.",
     },
     {
       id: "02",
-      heading: "Team Management",
+      heading: "Develop",
       desc: "Lorem ipsum dolor sit amet, provid adipisicing elit. Aliquam dolorum nu illo similique! Impedit.",
     },
     {
       id: "03",
-      heading: "Creative Mind",
+      heading: "Deliver",
       desc: "Lorem ipsum dolor sit amet, provid adipisicing elit. Aliquam dolorum nu illo similique! Impedit.",
     },
   ];
--- src/components/home/homes/Home.jsx
+++ src/components/home/homes/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -12,23 +13,32 @@ const Home = () => {
           </div>
           <div className="right topMarign">
             <h1>
-              I AM A <br />
-              SOFTWARE ENGINEER
+              HELLO, <br /> I AM ADRIAN KOLEV
             </h1>
+            <h3>Software engeneer</h3>
             <div className="SocailIcon">
               <i className="fab fa-facebook-f facebook"></i>
               <i className="fab fa-instagram instagram"></i>
-              <i className="fab fa-twitter twitter"></i>
-              <i className="fab fa-youtube youtube"></i>
-              <i className="fab fa-pinterest pinterest"></i>
-              <i className="fab fa-dribbble dribbble"></i>
+              <a
+                href="https://www.linkedin.com/in/adrkolev/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-linkedin linkedin"></i>
+              </a>
+              {/* <i className="fab fa-twitter twitter"></i> */}
+              {/* <i className="fab fa-youtube youtube"></i> */}
+              {/* <i className="fab fa-pinterest pinterest"></i> */}
+              {/* <i className="fab fa-dribbble dribbble"></i> */}
             </div>
             <p>
-              Lorem ipsum suspendisse ultrices gravida. Risus commodo viverra
-              maecenas accumsan lacus vel facilisis. eiusmod tempor incididunt
-              ut labore et dolore magna aliqua.
+              My passion is developing successfull projects. I achive it by
+              specializing in Software Engineering, Marketing Strategy & User
+              Experience.
             </p>
-            <button className="primary-btn">Contact Us</button>
+            <Link to="/contact">
+              <button className="primary-btn">Contact Me</button>
+            </Link>
           </div>
         </div>
       </section>
